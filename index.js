449cfd8ddef5b8a8778a8ae25abecb697fae207b
Chris Aquino <aquino.chris@pm.me>
2022-10-24 18:02:51 -0400

    Add simple onclick in HTML

---
+++ index.js
@@ -0,0 +1,11 @@
+const BUTTON_SELECTOR=`[data-id="target"]`;
+const theButton = document.querySelector(BUTTON_SELECTOR);
+
+console.log(theButton);
+
+// 1. add a simple alert when I click
+//theButton
+
+// 2. move that console.log to its own function
+
+
